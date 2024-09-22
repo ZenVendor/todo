1f20ee0708b3aceab8d6627dcd96ca8aa9912604
Zen Vendor <zen.vendor@mixbox.pl>
2024-09-22 20:56:45 +0200

    use tabwriter for list

--- main.go
+++ main.go
@@ -5,15 +5,17 @@ import (
 	_ "embed"
 	"fmt"
 	"log"
+	"os"
 	"slices"
+	"text/tabwriter"
 	"time"
 )
 
 const VERSION = "0.9.1"
 
-const C_WHITE = "\033[37m"
+const C_WHITE = "\033[37;0;00m"
 const C_GREY = "\033[38;5;7m"
-const C_RED = "\033[38;5;9m"
+const C_RED = "\033[38;5;09m"
 const C_ORANGE = "\033[38;5;214m"
 const C_YELLOW = "\033[38;5;226m"
 const C_GREEN = "\033[38;5;40m"
@@ -175,26 +177,31 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("%s tasks: %d\n", MapArgumentDescription()[sw], count)
+		fmt.Printf("%s%s tasks: %d%s\n", C_BOLD, MapArgumentDescription()[sw], count, C_RESET)
+
+        w := tabwriter.NewWriter(os.Stdout, 4, 0, 2, ' ', 0)
+        //fmt.Fprintf(w, "%s\tID\tGroup\tStatus\tDue\tDescription%s\n", C_BOLD, C_RESET)
 
 		for _, t := range tl {
+            lColor := C_WHITE
 			tStatus := "Open"
+            
 			if t.Done == 0 && t.Due.Valid && t.Due.Time.Before(time.Now()) {
-				tStatus = Color("Overdue", C_RED, true)
+				tStatus = "Overdue"
+                lColor = C_RED
 			}
 			if t.Done == 1 {
 				tStatus = "Closed"
+                lColor = C_GREEN
 			}
-			if t.Done == 0 {
-				if !t.Due.Valid {
-					fmt.Printf("\t%s :: %s %d: %s\n", t.Group.Name, tStatus, t.Id, t.Description)
-				} else {
-                    fmt.Printf("\t%s :: %s %d: %s, due: %s\n", t.Group.Name, tStatus, t.Id, t.Description, HumanDue(t.Due.Time, conf.DateFormat))
-				}
-			} else {
-				fmt.Printf("\t%s :: %s %d: %s, completed: %s\n", t.Group.Name, tStatus, t.Id, t.Description, t.Completed.Time.Format(conf.DateFormat))
-			}
+            tDue := HumanDue(t.Due.Time, conf.DateFormat)
+            if !t.Due.Valid {
+                tDue = ""
+            }
+
+            fmt.Fprintf(w, "%s\t%d\t%s\t%s\t%s\t%s%s\n", lColor, t.Id, t.Group.Name, tStatus, tDue, t.Description, C_RESET)
 		}
+        w.Flush()
 	}
 	if cmd == CMD_SHOW {
 		var t Task
--- parsers.go
+++ parsers.go
@@ -123,20 +123,20 @@ func MapArgumentDescription() map[int]string {
 }
 
 func MapPriorityValues() map[string]int {
-    return map[string]int{
-        "urgent":       0,
-        "important":    25,
-        "standard":     50,
-        "low":          75,
-    }
+	return map[string]int{
+		"urgent":    0,
+		"important": 25,
+		"standard":  50,
+		"low":       75,
+	}
 }
 func MapPriorityStrings() map[int]string {
-    return map[int]string{
-        0:  "urgent",
-        25: "important",
-        50: "standard",
-        75: "low",
-    }
+	return map[int]string{
+		0:  "urgent",
+		25: "important",
+		50: "standard",
+		75: "low",
+	}
 }
 
 type ArgVal struct {
@@ -246,41 +246,41 @@ func (vals ArgVals) ReadValue(cmd, sw int, dateFormat string) interface{} {
 
 func HumanDue(due time.Time, dateFormat string) string {
 
-    today, _ := time.Parse(dateFormat, time.Now().Format(dateFormat))
-    _, tWeek := today.ISOWeek()
-    _, dueWeek := due.ISOWeek()
+	today, _ := time.Parse(dateFormat, time.Now().Format(dateFormat))
+	_, tWeek := today.ISOWeek()
+	_, dueWeek := due.ISOWeek()
 
-    days := int(due.Sub(today).Hours() / 24)
-    weeks := dueWeek - tWeek
-    if weeks < 0 {
-        weeks = 52 + weeks
-    }
-    months := due.Month() - today.Month()
-    if months < 0 {
-        months = 12 + months
-    }
+	days := int(due.Sub(today).Hours() / 24)
+	weeks := dueWeek - tWeek
+	if weeks < 0 {
+		weeks = 52 + weeks
+	}
+	months := due.Month() - today.Month()
+	if months < 0 {
+		months = 12 + months
+	}
 
-    result := due.Format(dateFormat)
+	result := due.Format(dateFormat)
 
-    switch { 
-    case days < 0:
-        break
-        case days == 0: 
-        result = "today"
-    case days == 1:
-        result = "tomorrow"
-    case days == 2:
-        result = "in two days"
-    case weeks == 0:
-        result = "this week"
-    case weeks == 1:
-        result = "next week"
-    case weeks == 2:
-        result = "in two weeks"
-    case months == 0:
-        result = "this month"
-        case months == 1: 
-        result = "next month"
-    }
-    return result
+	switch {
+	case days < 0:
+		break
+	case days == 0:
+		result = "today"
+	case days == 1:
+		result = "tomorrow"
+	case days == 2:
+		result = "in two days"
+	case weeks == 0:
+		result = "this week"
+	case weeks == 1:
+		result = "next week"
+	case weeks == 2:
+		result = "in two weeks"
+	case months == 0:
+		result = "this month"
+	case months == 1:
+		result = "next month"
+	}
+	return result
 }
